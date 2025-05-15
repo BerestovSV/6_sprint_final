eca1181fc729ed6d1fad4d4033fcc14a309b1892
Berestov Semen <berestovsemen@gmail.com>
2025-05-15 17:45:21 +1000

    внесены исправления в соответствие с полученными результатми автотеста

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -63,7 +63,7 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write([]byte(convertedText))
 }
